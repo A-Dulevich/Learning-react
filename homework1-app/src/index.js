7c120b90824d8a12cb80eeb74bcb49ef1ddda721
Alex Dav <fcdav0721@gmail.com>
2021-04-06 11:27:52 +0300

    delete All. Create a new App (work with function)

--- homework1-app/src/index.js
+++ homework1-app/src/index.js
@@ -1,35 +1,24 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import reactDom from 'react-dom';
-import Header from './header/header'
-import './header/style.css'
-import './IT-MyFreedom/style.css'
-import Main from './main/main'
-import  './why are we block/style.css'
-import './footer/style.css'
-import Footer from './footer'
-
-// import {render} from 'react-dom';
-// import {Container} from './container/container'
-// function Por () {
-    
-//     return (
-//        <header>
-//       <Container/>
-//        </header>
-//     )
-    
-// }
-// render (<Por/>, document.getElementById('root'));
-class App extends React.Component{
-    render() {
-        return (
-            <body>
-        <Header/>
-         <Main/> 
-         <Footer/>
-        </body>
-        )
-    }
+function Article () {
+    const body = <section>body</section>
+    return (
+        <div>
+            <h2>title</h2>
+            {body}
+            <h3>creation date:{(new Date()).toDateString()}</h3>
+        </div>
+    )
 }
-
-reactDom.render(<App/>, document.getElementById('root'));
+function App () {
+    return (
+        <div>
+            <h1>
+                App name
+            </h1>
+            <Article/>
+        </div>
+    )
+}
+render(<App/>, document.getElementById('root'));
